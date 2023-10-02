539708276d23985d840c9a2f2e9d225a2fcee881
himanshee02 <himanshiruparelia02@gmail.com>
2023-10-02 14:49:43 +0530

    Newest comment

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 def main(msg):
-    // Adding the second comment to this file.
-    // Print a message
+    #Another comment
+    # Adding the second comment to this file.
+    # Print a message
     print(msg)
+
 
 main("Hello People")
